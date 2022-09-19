837cacb319d2c335c9433dc7a524a125a7834f1b
Volodymyr Budnyi <volodymyr.budnyi@gmail.com>
2022-09-19 16:50:41 +0200

    Load all activities data

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 const ANALYTICS_URL = 'https://connect.garmin.com/modern/analytics';
+const ANALYTICS_PAGE_SELECTED = window.location.href === ANALYTICS_URL;
 
 const initialPageLoadInterval = setInterval(function () {
     const menuElement = document.getElementsByClassName('main-nav-group dashboards')[0];
@@ -6,6 +7,7 @@ const initialPageLoadInterval = setInterval(function () {
     if (menuElement) {
         addIconsSource();
         addAnalyticsMenuItem(menuElement);
+        initAnalyticsPage();
 
         clearInterval(initialPageLoadInterval);
     }
@@ -20,7 +22,7 @@ function addIconsSource() {
 }
 
 function addAnalyticsMenuItem(menuElement) {
-    const activeTabStyleClass = window.location.href === ANALYTICS_URL ? 'active' : '';
+    const activeTabStyleClass = ANALYTICS_PAGE_SELECTED ? 'active' : '';
 
     const analyticsItem = document.createElement("li");
     analyticsItem.classList.add('main-nav-item');
@@ -32,4 +34,45 @@ function addAnalyticsMenuItem(menuElement) {
     `;
     
     menuElement.appendChild(analyticsItem);
+}
+
+function initAnalyticsPage() {
+    if (!ANALYTICS_PAGE_SELECTED) {
+        return;
+    }
+
+    loadData().then((data) => {
+        console.log(data);
+    });
+}
+
+async function loadData() {
+    let page = 1;
+    let activities = [];
+    let allPagesLoaded = false;
+
+    do {
+        const response = await loadPageData(page++);
+        activities = [...activities, ...response];
+        allPagesLoaded = response.length === 0;
+    } while (!allPagesLoaded);
+
+    return activities;
+}
+
+async function loadPageData(page) {
+    const limit = 100;
+    const start = (page - 1) * limit;
+
+    const url = `https://connect.garmin.com/activitylist-service/activities/search/activities?limit=${limit}&start=${start}`;
+
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'di-backend': 'connectapi.garmin.com',
+            'authorization': 'Bearer ' + JSON.parse(localStorage.getItem('token')).access_token
+        }
+    });
+
+    return response.json();
 }
