db021a20b83ac036b40b096ae5a18cd07cf090b1
Volodymyr Budnyi <volodymyr.budnyi@gmail.com>
2022-11-28 19:02:15 +0100

    Fix styles for monthly summary

--- src/app/components/progress-circle/progress-circle.component.ts
+++ src/app/components/progress-circle/progress-circle.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { WidgetSize } from 'src/app/enums/widget-size.enum';
 
 @Component({
   selector: 'app-progress-circle',
@@ -11,6 +12,8 @@ export class ProgressCircleComponent {
   @Input() subValue: string;
   @Input() title: string;
   @Input() percent: number;
+  @Input() isTotal: boolean;
+  @Input() widgetSize: WidgetSize;
 
   get circleOffset(): number {
     return 360 - ((this.percent > 100 ? 100 : this.percent) * 2.1);
